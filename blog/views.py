d83185826abe4c33e2a6bbde023f0086192724d9
tono <toninho_fpt@hotmail.com>
2016-04-01 14:50:24 -0600

    Identacion

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request,'blog/post_list.html',{'post': posts})
+        return render(request,'blog/post_list.html',{'post': posts})
